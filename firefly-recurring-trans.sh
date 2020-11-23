cdbd6432aaa0fec508957358a8a2d74d3c2f719e
Kwbmm <kwbmm1990@gmail.com>
2020-11-23 21:25:50 +0000

    Fix firefly recurring transactions script

--- firefly-recurring-trans.sh
+++ firefly-recurring-trans.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
-docker exec firefly /usr/local/bin/php /var/www/firefly-iii/artisan firefly-iii:cron
-docker exec firefly /usr/local/bin/php /var/www/firefly-iii/artisan cache:clear
+docker exec --user www-data firefly /usr/local/bin/php /var/www/html/artisan firefly-iii:cron
+docker exec --user www-data firefly /usr/local/bin/php /var/www/html/artisan cache:clear
