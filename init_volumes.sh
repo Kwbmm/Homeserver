8770c1204fe0f2e12142c7d010ee09440399d041
Marco Ardizzone <kwbmm1990@gmail.com>
2024-02-19 00:59:37 +0100

    Fix init_volumes

--- init_volumes.sh
+++ init_volumes.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 cd ~
-mkdir -p volumes/homeassistant
 mkdir -p volumes/traefik-letsencrypt
 mkdir -p volumes/ttrss-plugins
 mkdir -p volumes/ttrss-db
+mkdir -p volumes/freshrss
+mkdir -p volumes/freshrss/data
+mkdir -p volumes/freshrss/extensions
